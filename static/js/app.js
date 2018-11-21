8576dadb1297db9025cec00ce65ad12dacae5a6c
pogomike <mpogatchnik@gmail.com>
2018-11-21 09:13:12 -0600

    intial commit

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+// from data.js
+var tableData = data;
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+data.forEach((UFOReport) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
