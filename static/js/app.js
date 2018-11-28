cb619d0f2516284f75a8c3e12bf34c78cdfbc06b
pogomike <mpogatchnik@gmail.com>
2018-11-27 21:15:29 -0600

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,42 @@
-// from data.js
-var tableData = data;
-
-// Get a reference to the table body
+//from data.js
+//Import Data into Table onto website
 var tbody = d3.select("tbody");
+var tableData = data;
+//console.log(tableData);
 
-// Console.log the weather data from data.js
-console.log(data);
-
-data.forEach((UFOReport) => {
-    var row = tbody.append("tr");
-    Object.entries(UFOReport).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
+data.forEach(function(weatherReport) {
+  console.log(weatherReport);
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = tbody.append("td");
+    cell.text(value);
   });
+});
 
+// User input to filer by date
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+ var selectTable = d3.select("tbody");
+ // Prevent the page from refreshing
+ d3.event.preventDefault();
+ selectTable.html("");
+
+ var inputElement = d3.select("#datetime");
+
+// Get the value property of the input element
+ var inputDate = inputElement.property("value");
+
+ tableData.forEach(function(row) {
+     if(row.datetime == inputDate) {
+         var newRow = selectTable.append("tr");
+         Object.entries(row).forEach(function([key, value]) {
+             var cell = newRow.append("td");
+             cell.text(value);
+         });
+     }
+ })
+});
